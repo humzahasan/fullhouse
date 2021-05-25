1f1f76c22190d23a74d1f4481e458eaee58da38c
Humza Hasan <humzahasan2101@gmail.com>
2021-05-25 23:37:01 +0530

    fix : lots of fixes

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,51 +2,138 @@ import {Avatar, TextField} from '@material-ui/core';
 import './Chat.css';
 import React, {useState} from 'react';
 import {useEffect} from 'react';
-import {BiMessageRoundedAdd} from 'react-icons/bi';
 import {useParams} from 'react-router-dom';
-import {projectFirestore} from '../../config/firebase';
+import {projectFirestore, timestamp} from '../../config/firebase';
 
 const Chat = () => {
-  const [roomData, setRoomData] = useState({});
-  const {roomId} = useParams();
   const roomRef = projectFirestore.collection('chatrooms');
+  const [roomData, setRoomData] = useState(null);
+  const {roomId} = useParams();
+  const [messageInput, setMessageInput] = useState('');
+  const [roomMessages, setRoomMessages] = useState([]);
 
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log('Yo BABY', messageInput);
+    projectFirestore.collection(roomData.name).add({
+      roomData: roomData,
+      username: 'USER',
+      message: messageInput,
+      sentAt: timestamp(),
+    });
+    setMessageInput('');
+  };
   useEffect(() => {
     async function loadRoomData() {
       let note = await roomRef.doc(roomId).get();
       note = note.data();
       setRoomData(note);
+      
+      loadRoomMessages()
     }
+
+    async function loadRoomMessages() {
+      
+    }
+    //console.log(roomMessages);
     loadRoomData();
-  }, [roomId, roomRef]);
+  }, []);
 
   return (
-    <div className='chat'>
-      <div>
-        <p>This is the parent div which contains the div we are testing</p>
-
-        <div>
-          <p>
-            This example shows that changing the background color of a div does
-            not affect the border and margin of the div.
-          </p>
+    <>
+      {roomData && (
+        <div className='chat'>
+          <div className='chat__info'>
+            <h3>{roomData.name}</h3>
+            <h3>{String(roomData.createdAt)}</h3>
+          </div>
+          <div className='chat__body'>
+            <div className='chat__messagesent'>
+              <Avatar>A</Avatar>
+              <div className='chat_user'>
+                <p>you</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque rerum illum excepturi nam assumenda, tempora saepe.
+                  Molestias, deserunt iste. Perspiciatis, soluta. Laborum earum
+                  doloribus unde vel adipisci in hic dignissimos.
+                </p>
+              </div>
+            </div>
+            <div className='chat__messagereceived'>
+              <Avatar>A</Avatar>
+              <div className='chat_user'>
+                <p>you</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque rerum illum excepturi nam assumenda, tempora saepe.
+                  Molestias, deserunt iste. Perspiciatis, soluta. Laborum earum
+                  doloribus unde vel adipisci in hic dignissimos.
+                </p>
+              </div>
+            </div>
+            <div className='chat__messagesent'>
+              <Avatar>A</Avatar>
+              <div className='chat_user'>
+                <p>you</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque rerum illum excepturi nam assumenda, tempora saepe.
+                  Molestias, deserunt iste. Perspiciatis, soluta. Laborum earum
+                  doloribus unde vel adipisci in hic dignissimos.
+                </p>
+              </div>
+            </div>
+            <div className='chat__messagereceived'>
+              <Avatar>A</Avatar>
+              <div className='chat_user'>
+                <p>you</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque rerum illum excepturi nam assumenda, tempora saepe.
+                  Molestias, deserunt iste. Perspiciatis, soluta. Laborum earum
+                  doloribus unde vel adipisci in hic dignissimos.
+                </p>
+              </div>
+            </div>
+            <div className='chat__messagesent'>
+              <Avatar>A</Avatar>
+              <div className='chat_user'>
+                <p>you</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque rerum illum excepturi nam assumenda, tempora saepe.
+                  Molestias, deserunt iste. Perspiciatis, soluta. Laborum earum
+                  doloribus unde vel adipisci in hic dignissimos.
+                </p>
+              </div>
+            </div>
+            <div className='chat__messagereceived'>
+              <Avatar>A</Avatar>
+              <div className='chat_user'>
+                <p>you</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque rerum illum excepturi nam assumenda, tempora saepe.
+                  Molestias, deserunt iste. Perspiciatis, soluta. Laborum earum
+                  doloribus unde vel adipisci in hic dignissimos.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='chat__messageInput'>
+            <form onSubmit={sendMessage}>
+              <TextField
+                id='chat-message'
+                label='Type a message'
+                value={messageInput}
+                onChange={(e) => setMessageInput(e.target.value)}
+              />
+            </form>
+          </div>
         </div>
-      </div>
-      {/* 
-      <div className='chat__body'>
-        <div className='chat__messages'>
-          <Avatar>A</Avatar>
-        </div>
-        <div className='chat__form'>
-          <form>
-            <TextField id='standard-basic' label='Enter message' />
-            <button>
-              <BiMessageRoundedAdd fontSize='1.5rem' />
-            </button>
-          </form>
-        </div>
-      </div> */}
-    </div>
+      )}
+    </>
   );
 };
 
